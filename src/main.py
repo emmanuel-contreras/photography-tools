d20b8357c2fe3dc1be79df87756018fa0eeb2ab9
emmanuel-contreras <emmanuel.contreras@wisc.edu>
2022-09-03 16:47:50 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
 from pathlib import Path
+from tqdm import tqdm
 
 path_scans = Path(r"D:\Photos\original_scans")
 
 path_output = path_scans / "output"
+
+from split_multi_scanned_photos import split_multi_scanned_photos
+
+
+list_path_tiffs = list(path_scans.glob("*.tif"))
+
+for path_im in tqdm(list_path_tiffs[:]):
+    split_multi_scanned_photos(path_im, path_output=path_output)
+
