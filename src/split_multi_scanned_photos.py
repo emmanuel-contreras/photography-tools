fixed rotation issue in deskew section

src/split_multi_scanned_photos.py
@@ -11,6 +11,11 @@ from skimage.transform import rotate
 from skimage.filters import threshold_otsu
 from skimage.morphology import label
 
+import pandas as pd
+
+from skimage.filters import gaussian, difference_of_gaussians
+
+from hampel import hampel
 from tqdm import tqdm
 
 #%%
@@ -18,7 +23,7 @@ from tqdm import tqdm
 def split_multi_scanned_photos(path_im: Path, 
                                       path_output : Path=None,
                                       region_threshold : int=1e6,
-                                      pad : int=50,
+                                      pad : int=25,
                                       deskew : bool=True,
                                       debug : bool=False):
     """
@@ -66,9 +71,17 @@ def split_multi_scanned_photos(path_im: Path,
     if debug:
         plt.imshow(im_intensity)
         plt.show()
-    
+        
+    # im_intensity = gaussian(im_intensity, (2,10))
+    # plt.imshow(im_intensity2)
+    # plt.show()
+        
     # create binary based on otsu, bg vs fg
-    mask_binary = im_intensity > threshold_otsu(im_intensity)
+    im_intensity_exp = im_intensity**2
+    # plt.imshow((im_intensity_exp) > threshold_otsu((im_intensity_exp)))
+    
+    mask_binary = im_intensity_exp > threshold_otsu(im_intensity_exp)
+    # mask_binary = im_intensity > threshold_otsu(im_intensity)
     if debug:
         plt.imshow(mask_binary)
         plt.show()
@@ -96,39 +109,89 @@ def split_multi_scanned_photos(path_im: Path,
         im_rgb = im_norm[max(minr-pad,0):min(maxr+pad,max_rows), max(minc-pad,0):min(maxc+pad,max_cols),:]
         im_orig = im[max(minr-pad,0):min(maxr+pad,max_rows), max(minc-pad,0):min(maxc+pad,max_cols),:]
     
-        if debug:
-            # find better way to get the angle?
-            plt.title("image without rotation")
-            plt.imshow(im_rgb)
-            plt.show()
-            
-            plt.title("image rotated")
-            plt.imshow(rotate(im_rgb, angle=r.orientation, cval=np.max(im_rgb)))
-            plt.show()
         
         if deskew:
-            im_orig = rotate(im_orig, 
-                            angle=r.orientation, 
-                            cval=np.max(im_orig), # to make white or as close to white
+            
+            # find bottom edge to fit a line to
+            mask_image = r.image
+            
+            # determine col indices with values by projecting
+            values_col = mask_image.sum(axis=1)
+            values_col = np.argwhere(values_col)
+            
+            # pick 1/4 of points in the middle to avoid outlier at edges
+            quarter = len(values_col) // 4
+            middle = len(values_col)//2
+           
+            values_col = values_col[middle-quarter:middle+quarter]
+            
+            
+            # determine max idx of values in each col
+            val_rows =[]
+            for idx_col in values_col:
+                pass
+                col_indexes = np.argwhere(mask_image[:,idx_col])
+                val_rows.append(np.max(col_indexes))
+                
+            # hampel filter to reduce outliers in y
+            val_rows = hampel(pd.Series(val_rows),
+                              window_size=100,
+                              n=1,
+                              imputation=True)
+            
+            # fit 1d line
+            fit_line = np.polyfit(values_col.squeeze(), val_rows, deg=1)
+            
+            if debug:
+                plt.scatter(values_col, val_rows, s=1)
+            
+            # visualize linear fit
+            x = np.linspace(0,mask_image.shape[1],mask_image.shape[1])
+            y = fit_line[0]*x + fit_line[1]
+            if debug:
+                plt.plot(x,y)
+            angle = np.rad2deg(np.arctan(fit_line[0]))
+
+            # angle_in_degrees = r.orientation * (180/np.pi) + 90 
+            im_orig = rotate(im_orig,
+                            angle=angle, 
+                            cval=np.min(r.image), # to make white or as close to white
                             preserve_range=True)
-        
-        if not path_output:
+            if debug:
+                plt.title("image without rotation")
+                plt.imshow(im_rgb)
+                plt.show()
+                
+                plt.title("image rotated")
+                plt.imshow(rotate(im_rgb, 
+                                  angle=angle, 
+                                  cval=np.max(im_rgb)))
+                plt.show()
+    
+            
+        if not debug and not path_output:
             path_output = path_im.parent / 'output'
             path_output.mkdir(exist_ok=True)
             
         # finally save image
-        tifffile.imwrite( path_output / f"{path_im.stem}_{idx}.tiff", im_orig.astype(np.uint16))
-
+        if not debug:
+            tifffile.imwrite( path_output / f"{path_im.stem}_{idx}.tiff", im_orig.astype(np.uint16))
+#%%
 if __name__ == "__main__":
     
+    #%%
     # test images
-    list_path_tiffs = [
-        Path(r"19871213-152.tif"),
-        Path(r"19871213-156.tif")
-        ]
+    # list_path_tiffs = [
+    #     Path(r"19871213-152.tif"),
+    #     Path(r"19871213-156.tif")
+    #     ]
     
-    for path_im in tqdm(list_path_tiffs[:]):
+    path_images = Path(r"./test_images")
+    list_path_images = list(path_images.glob("*.tif"))
+    
+    
+    for path_im in tqdm(list_path_images[:]):
         pass
-        split_multi_scanned_photos(path_im, deskew=True)
+        split_multi_scanned_photos(path_im, deskew=True, debug=True)
 
 